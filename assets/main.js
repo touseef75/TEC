43482f81f81701788da866781e5382f3acb44140
touseef75 <imrantouseef776@gmail.com>
2021-10-16 01:41:58 -0700

    set

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+   let h3 = document.getElementById("h3");
+        let h4 = document.getElementById("h4");
+        let h5 = document.getElementById("h5");
+        let h6 = document.getElementById("h6");
+        
+
+        let audio = document.getElementById("audio");
+        h3.onclick = function(){
+            audio.play();
+        }
+        h4.onclick = function () {
+                audio.play();
+                
+            }
+            h5.onclick = function () {
+                    audio.play();
+                }
+                h6.onclick = function () {
+                        audio.play();
+                    }
